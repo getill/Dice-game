6f6c89b9c5fef31c4c5caa93c29cad8ef957f5c5
Théo Lemray <theo.lemray@gmail.com>
2022-11-09 16:11:31 +0100

    Player display style is now in a function to optimize the code ! + Roll dice change to You loose.. when.. you loose...

--- script.js
+++ script.js
@@ -11,6 +11,16 @@ let roundP2 = document.getElementById("roundP2");
 //------------------------Game variables---------------
 let activePlayer = 1;
 
+function switchPlayer() {
+  if (activePlayer == 1) {
+    player1.style.opacity = "1";
+    player2.style.opacity = "0.3";
+  } else {
+    player1.style.opacity = "0.3";
+    player2.style.opacity = "1";
+  }
+}
+
 // Roll logic
 let rollfunction = () => {
   // Generates a random number between 1 to 6
@@ -21,23 +31,27 @@ let rollfunction = () => {
   document.querySelector("img").setAttribute("src", randomImg);
 
   if (activePlayer == 1 && diceNumber > 1) {
+    roll.textContent = "ROLL DICE !";
     // puts the number into the current score
     let totalCurrentP1 = roundP1.textContent;
     // I had to put a "+" to make the addition possible beacause rounsP2.textContent is a string
     roundP1.textContent = +totalCurrentP1 + +diceNumber;
-    player1.style.opacity = "1";
-    player2.style.opacity = "0.3";
+    switchPlayer();
   } else if (activePlayer == 1 && diceNumber == 1) {
     // Switch player if dice = 1
+    roll.textContent = "You loose !";
     roundP1.textContent = "0";
     activePlayer = activePlayer + 1;
-    player1.style.opacity = "0.3";
-    player2.style.opacity = "1";
+    switchPlayer();
   } else if (activePlayer == 2 && diceNumber > 1) {
+    // Put score into P2 section
+    roll.textContent = "ROLL DICE !";
     let totalCurrentP2 = roundP2.textContent;
     roundP2.textContent = +totalCurrentP2 + +diceNumber;
   } else {
+    // Switch to player 1 if player 2 get "1"
     roundP2.textContent = "0";
+    roll.textContent = "You loose !";
     activePlayer = activePlayer - 1;
   }
 };
