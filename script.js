35183225041b90813b2dfbf1332412cf64d26625
Théo Lemray <theo.lemray@gmail.com>
2022-11-09 15:45:55 +0100

    roll logic development (changing player)

--- script.js
+++ script.js
@@ -1,44 +1,38 @@
-//------------------------ Variables---------------
+//------------------------Get element variables---------------
 let newGame = document.getElementById("new-game");
 let roll = document.getElementById("roll");
 let hold = document.getElementById("hold");
 let img = document.getElementById("img");
-let center = document.getElementById("center");
-let roundP1 = document.getElementById("roundP1");
-let roundP2 = document.getElementById("roundP2");
 let globalP1 = document.getElementById("globalP1");
+let roundP1 = document.getElementById("roundP1");
+let globalP2 = document.getElementById("globalP2");
+let roundP2 = document.getElementById("roundP2");
 
-let player = "p1";
-function switchPlayer(currentPlayer) {
-  if (currentPlayer === "p1") {
-    player = "p2";
-  } else {
-    player = "p1";
-  }
-}
+//------------------------Game variables---------------
+let activePlayer = 1;
 
-let randomNumber = () => {
-  return Math.floor(Math.random() * 6) + 1;
-};
-
-// P1 roll logic
+// Roll logic
 let rollfunction = () => {
   // Generates a random number between 1 to 6
-  let diceNumber = randomNumber();
-  // Generate a string with the img path, including a random number between
+  let diceNumber = Math.floor(Math.random() * 6) + 1;
+  // Generate a string with the img path, including a random number between then display the img
   let randomImg = "images/" + diceNumber + ".png";
+  // Used to get the img element then set the src attribute to randomImg (= images/1.png or any other numbers)
+  document.querySelector("img").setAttribute("src", randomImg);
 
-  if (diceNumber > 1) {
+  if (activePlayer == 1 && diceNumber > 1) {
     // puts the number into the current score
-    let totalCurrent = roundP1.textContent;
-    // Used to get the img element then set the src attribute to randomImg (= images/1.png or any other numbers)
-    document.querySelector("img").setAttribute("src", randomImg);
+    let totalCurrentP1 = roundP1.textContent;
     // I had to put a "+" to make the addition possible beacause rounsP2.textContent is a string
-    roundP1.textContent = +totalCurrent + +diceNumber;
-  } else {
-    // Switch player logic
-    switchPlayer(player);
-    console.log(player);
+    roundP1.textContent = +totalCurrentP1 + +diceNumber;
+    player1.style.opacity = "1";
+    player2.style.opacity = "0.3";
+  } else if (activePlayer == 1 && diceNumber == 1) {
+    // Switch player if dice = 1
+    roundP1.textContent = "0";
+    activePlayer = activePlayer + 1;
+    player1.style.opacity = "0.3";
+    player2.style.opacity = "1";
   }
 };
 
